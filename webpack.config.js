6768b6ebfbb54299aae52b019da53b97c74c6770
chanathep <39905610+chanathep@users.noreply.github.com>
2024-01-18 10:00:02 +0700

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  // The entry point of the web application
+  entry: './src/index.js',
+  // The output configuration of the web application
+  output: {
+    // The name of the output file
+    filename: 'main.js',
+    // The path of the output folder
+    path: path.resolve(__dirname, 'dist'),
+    // The public path of the output folder
+    publicPath: ''
+  },
+  // The development server configuration of the web application
+  devServer: {
+    // The port of the development server
+    port: 3000,
+    // The content base of the development server
+    contentBase: './dist'
+  }
+};
